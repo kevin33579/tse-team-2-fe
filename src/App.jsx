5950c036fe527547447930d7e491da33eee56130
micoazmi <micoazmi@yahoo.com>
2025-06-07 06:49:03 +0700

    Navbar Component and button

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/navbar"
+
+
+function App() {
+
+
+  return (
+    <>
+     <Navbar></Navbar>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import "./navbar.css";
+import Button from "./Button";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-left">
+        <img src="/logo.png" alt="Logo" className="logo" />
+        <span className="brand">Otomobil</span>
+      </div>
+      <div className="navbar-right">
+        <Button
+          label="Sign Up"
+          variant="secondary"
+          onClick={() => alert("Sign Up clicked")}
+        />
+        <Button
+          label="Login"
+          variant="primary"
+          onClick={() => alert("Login clicked")}
+        />
+      </div>
+    </nav>
+  );
+}
