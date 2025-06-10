d2f640ea74c902012c0037f790f938316181a497
kevin33579 <kingkiller977@gmail.com>
2025-06-10 21:20:38 +0700

    update using mui

--- src/pages/ResetPasswordEmail.jsx
+++ src/pages/ResetPasswordEmail.jsx
@@ -1,38 +1,100 @@
-import FormInput from "../components/FormInput";
+import React from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 import Navbar from "../components/Navbar";
-import Button from "../components/Button";
-import 'typeface-montserrat';
-import "./ResetPassword.css";
+import "@fontsource/montserrat";
 
 const ResetPasswordEmail = () => {
   return (
     <>
       <Navbar />
-      <div className="reset-password-page">
-        <div className="reset-password-container">
-          <h2 className="reset-password-title">Reset Password</h2>
-          <p className="reset-password-subtext">
+      <Box
+        sx={{
+          minHeight: "100vh",
+          padding: "40px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          fontFamily: "Montserrat",
+        }}
+      >
+        <Box
+          sx={{
+            padding: "40px",
+            borderRadius: "20px",
+            maxWidth: "600px",
+            width: "100%",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "flex-start",
+            textAlign: "left",
+            marginTop: "60px",
+            fontFamily: "Montserrat",
+          }}
+        >
+          <Typography
+            variant="h5"
+            sx={{ fontWeight: "medium", marginBottom: "10px", width: "100%" }}
+          >
+            Reset Password
+          </Typography>
+
+          <Typography
+            variant="body1"
+            sx={{ fontWeight:'light',color: "#666", marginBottom: "30px", width: "100%" }}
+          >
             Send OTP code to your email address
-          </p>
+          </Typography>
 
-          <FormInput placeholder="Email" inputName="email" />
+          <TextField
+            label="Email"
+            name="email"
+            fullWidth
+            variant="outlined"
+            sx={{ marginBottom: "30px" }}
+          />
 
-          <div className="reset-password-actions">
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "flex-end",
+              gap: "20px",
+              width: "100%",
+            }}
+          >
             <Button
-              className="btn cancel"
-              label="Cancel"
-              variant="secondary"
+              variant="outlined"
+              size="large"
+              sx={{
+                borderColor: "#790B0A",
+                color: "#790B0A",
+                textTransform: "none",
+                width: "140px",
+                height: "38px",
+              }}
               onClick={() => alert("Cancel clicked")}
-            />
+            >
+              Cancel
+            </Button>
             <Button
-              className="btn confirm"
-              label="Confirm"
-              variant="primary"
+              variant="contained"
+              size="medium"
+              sx={{
+                backgroundColor: "#790B0A",
+                color: "#fff",
+                textTransform: "none",
+                width: "140px",
+                height: "38px",
+              }}
               onClick={() => alert("Confirm clicked")}
-            />
-          </div>
-        </div>
-      </div>
+            >
+              Confirm
+            </Button>
+          </Box>
+        </Box>
+      </Box>
     </>
   );
 };
