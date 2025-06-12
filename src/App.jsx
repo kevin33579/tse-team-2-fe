930f6e605f684288fbe8b6069f8dfbd2623da367
kevin33579 <kingkiller977@gmail.com>
2025-06-12 20:14:26 +0700

    add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Checkout from "./pages/Checkout";
 import DetailKelas from "./pages/DetailKelas";
 import Landing from "./pages/Landing";
 import ListMenuKelas from "./pages/ListMenuKelas";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+
 function App() {
   return (
-    <>
-      {/* <DetailKelas></DetailKelas> */}
-      {/* <Login></Login> */}
-      <Register></Register>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/list-menu-kelas" element={<ListMenuKelas />} />
+        <Route path="/list-menu-kelas/:id" element={<DetailKelas />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
